638c94ac98d3d2b15b24efc4edaa6083b8358885
Subhalaxmi Rout <subhalaxmirout78@gmail.com>
2021-03-11 18:14:52 +0530

    11/03

---
+++ DemoWeb/Webapp.Web/Scripts/UserRoleScript.js
@@ -0,0 +1,32 @@
+﻿function PopulateUserRoleGrid() {
+    var url = DemoAppWebAPIURL + "api/UserRole/GetAll";
+    // prepare the data
+    var source =
+    {
+        datatype: "json",
+        datafields: [
+            { name: 'Id', type: 'int' },
+            { name: 'UserId' },
+            { name: 'RollId' },
+            { name: 'IsActive', type: 'bool' }
+        ],
+        id: 'Id',
+        url: url,
+        root: 'data'
+    };
+    var dataAdapter = new $.jqx.dataAdapter(source);
+
+    $("#UserRoleGrid").jqxGrid(
+        {
+
+            
+            source: dataAdapter,
+            columnsresize: true,
+            columns: [
+                { text: 'Id', dataField: 'Id', width: 150 },
+                { text: 'UserId', dataField: 'UserId', width: 150 },
+                { text: 'RollId', dataField: 'RollId', width: 150 },
+                { text: 'IsActive', dataField: 'IsActive' }
+               ]
+        });
+}
---
+++ DemoWeb/Webapp.WebAPI/Controllers/UserRoleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Webapp.Data;
+
+namespace Webapp.WebAPI.Controllers
+{
+    public class UserRoleController : ApiController
+    {
+        private DemoWebEntities db = new DemoWebEntities();
+
+        [HttpGet]
+        public dynamic GetAll()
+        {
+            return db.UserRoles.ToList();
+        }
+    }
+}
